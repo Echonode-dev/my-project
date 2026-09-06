2c250581bc5b50761871207d5ec66bbd5e03d66c
Echonode-dev <rinki2uw7@gmail.com>
2026-09-06 15:47:42 +0545

    Fix: Add missing View import to MainActivity

--- app/src/main/java/com/unhook/app/ui/MainActivity.java
+++ app/src/main/java/com/unhook/app/ui/MainActivity.java
@@ -7,6 +7,7 @@ import android.app.usage.UsageStatsManager;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.view.Gravity;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
